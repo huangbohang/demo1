30c39ef27e17d68ecf85cd77c92f3519b6f901cc
huangbohang <hbh1092142160@163.com>
2024-08-27 15:03:22 +0800

    deploy

---
+++ js/script.js
@@ -0,0 +1,30 @@
+if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
+    // new WOW().init();
+};
+
+$(function(){
+	//通用
+	
+	
+	// $('.gh').click(function(){
+	// 		$('.layout').toggleClass('selected');
+	// 		$('.naver ul').slideToggle();
+	// 	});
+		
+	
+	// if($(window).scrollTop()>0){
+	// 	$(".layout").addClass('on')
+	// }else{
+	// 	$(".layout").removeClass('on')
+	// }
+	
+	// $(window).scroll(function(){
+	// 	if($(window).scrollTop()>0){
+	// 		$(".layout").addClass('on')
+	// 	}else{
+	// 		$(".layout").removeClass('on')
+	// 	}
+	// })
+	
+		
+})
